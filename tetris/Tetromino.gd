d884bd273433376b469bab8d939d51773d754253
Eptwalabha <damiengabrielle@gmail.com>
2018-05-06 21:46:55 +0200

    random tetrominos shape

--- tetris/Tetromino.gd
+++ tetris/Tetromino.gd
@@ -1,13 +1,22 @@
 extends Node
 
+var shapes = [
+	[Vector2(0, 0), Vector2(1, 0), Vector2(2, 0), Vector2(0, 1)],	# L
+	[Vector2(0, 1), Vector2(1, 1), Vector2(2, 1), Vector2(0, 0)],	# J
+	[Vector2(0, 0), Vector2(1, 0), Vector2(2, 0), Vector2(3, 0)],	# I
+	[Vector2(0, 0), Vector2(1, 0), Vector2(2, 0), Vector2(1, 1)],	# T
+	[Vector2(0, 0), Vector2(1, 0), Vector2(1, 1), Vector2(2, 1)],	# S
+	[Vector2(0, 1), Vector2(1, 1), Vector2(1, 0), Vector2(2, 0)]	# Z
+]
+
 func _ready():
 	pass
 
 func init(x, y, spec):
 	x = randi() % 6
 	var type = randi() % 4
-	var blocks = [Vector2(0, 0), Vector2(1, 0), Vector2(2, 0), Vector2(0, 1)]
-	for block_spec in blocks:
+	var shape = shapes[randi() % shapes.size()]
+	for block_spec in shape:
 		var block = preload("res://tetris/Block.tscn").instance()
 		block.init(block_spec.x + x, block_spec.y - 1, type, self)
 		$Blocks.add_child(block)
