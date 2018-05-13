043a8d73df59a1c88992025962ce8f5db911500f
Eptwalabha <damiengabrielle@gmail.com>
2018-05-14 00:26:09 +0200

    improve real-time mechanic

--- world/game_modes/RealTime.gd
+++ world/game_modes/RealTime.gd
@@ -1,6 +1,8 @@
 extends "res://world/game_modes/GameMode.gd"
 
 var tick
+var wait_end_of_shooting = false
+var down_on_end_bullet = false
 
 func init(world, player):
 	self.world = world
@@ -12,8 +14,25 @@ func init(world, player):
 func player_shoot():
 	player.shoot(world)
 
+func player_start_shooting():
+	player_paused = true
+	wait_end_of_shooting = true
+
+func player_end_shooting():
+	player_paused = false
+	wait_end_of_shooting = false
+	if down_on_end_bullet:
+		_move_blocks_down(0.1)
+		down_on_end_bullet = false
+		tick.start()
+
 func grid_end_moving_blocks_down():
-	tick.start()
+	down_on_end_bullet = false
+	if not wait_end_of_shooting:
+		tick.start()
 
 func _on_tick_timeout():
-	_move_blocks_down(0.1)
+	if not wait_end_of_shooting:
+		_move_blocks_down(0.1)
+	else:
+		down_on_end_bullet = true
